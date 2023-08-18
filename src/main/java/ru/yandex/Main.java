6cac71d316024141defd5cfadbddee32e77a7a0d
Gafar0v <gafaroffis@yandex.ru>
2023-08-18 21:39:01 +0300

    find primes

--- src/main/java/ru/yandex/Main.java
+++ src/main/java/ru/yandex/Main.java
@@ -3,9 +3,10 @@ package ru.yandex;
 import ru.yandex.tasks.Task4DFS;
 import ru.yandex.tasks.Task5BFS;
 import ru.yandex.tasks.Task6BraceBalance;
+import ru.yandex.tasks.Task7PrimeNumbers;
 
 public class Main {
     public static void main(String[] args) {
-        Task6BraceBalance.selfCheck();
+        Task7PrimeNumbers.selfCheck();
     }
 }
--- src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
+++ src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
@@ -5,13 +5,36 @@ package ru.yandex.tasks;
 import java.util.Arrays;
 
 public class Task7PrimeNumbers {
-    public static int[] findPrimes (int N) {
+    public static int[] findPrimes(int N) {
         /*
          * 2 <= N <= 10^6
          * Выход: отсортированный массив всех простых чисел от 2 до N
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        boolean[] numbers = new boolean[N + 1];
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (!numbers[i]) {
+                for (int j = 2; (j * i) <= N; j++) {
+                    numbers[j * i] = true;
+                }
+            }
+        }
+        int n = 0;
+        for (int i = 2; i <= N; i++) {
+            if (!numbers[i]) {
+                n++;
+            }
+        }
+        int[] primes = new int[n];
+        int indx = 0;
+        for (int i = 2; i <= N; i++) {
+            if (!numbers[i]) {
+                primes[indx] = i;
+                indx++;
+            }
+        }
+        return primes;
     }
 
     public static void selfCheck() {
