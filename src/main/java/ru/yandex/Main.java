0353a4f48217fdf3699d44b631035a803c780af0
Gafar0v <gafaroffis@yandex.ru>
2023-08-18 18:51:07 +0300

    SelectionSort

--- src/main/java/ru/yandex/Main.java
+++ src/main/java/ru/yandex/Main.java
@@ -1,7 +1,9 @@
 package ru.yandex;
 
+import ru.yandex.tasks.Task2SelectionSort;
+
 public class Main {
     public static void main(String[] args) {
-
+        Task2SelectionSort.selfCheck();
     }
 }
--- src/main/java/ru/yandex/tasks/Task2SelectionSort.java
+++ src/main/java/ru/yandex/tasks/Task2SelectionSort.java
@@ -12,7 +12,20 @@ public class Task2SelectionSort {
          * Выход: отсортированный (сортировкой выбором!) numbers
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        for (int i = 0; i < numbers.size(); i++) {
+            int min = numbers.get(i);
+            int minIndx = i;
+            for (int j = i; j < numbers.size(); j++) {
+                if (numbers.get(j) < min) {
+                    min = numbers.get(j);
+                    minIndx = j;
+                }
+            }
+            int tmp = numbers.get(i);
+            numbers.set(i, min);
+            numbers.set(minIndx, tmp);
+        }
+        return numbers;
     }
 
     public static void selfCheck() {
